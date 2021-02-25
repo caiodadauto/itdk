21f25947352d3c179fdf70db7b26fd89aed8d14f
Caio Dadauto <caiodadauto@gmail.com>
2021-02-24 22:51:18 -0300

    add interface processing in graphs

--- itdk/itdk/graphs.py
+++ itdk/itdk/graphs.py
@@ -3,33 +3,10 @@ import os
 import numpy as np
 import pandas as pd
 import graph_tool as gt
-from numba import jit
 from graph_tool.draw import graph_draw
 
 from itdk.logger import create_logger
 from itdk.ases import get_unique_index
-
-
-@jit(nopython=True)
-def get_valid_nodes(n1, n2, n):
-    n = np.sort(n)
-    n1_idx = np.searchsorted(n, n1)
-    n2_idx = np.searchsorted(n, n2)
-
-    size_n = n.shape[0]
-    ext_n = np.zeros(size_n + 1, dtype=np.int64)
-    ext_n[0 : n.shape[0]] = n
-
-    n1_out = ext_n[n1_idx]
-    n2_out = ext_n[n2_idx]
-    n1_mask = n1_out == n1
-    n2_mask = n2_out == n2
-    mask = n1_mask & n2_mask
-
-    senders = n1[mask]
-    receivers = n2[mask]
-    all_valid_nodes = np.concatenate((senders, receivers))
-    return all_valid_nodes
 
 
 def draw_graph(locations, edges, file_name, ext="png", use_pos=False):
@@ -50,134 +27,115 @@ def draw_graph(locations, edges, file_name, ext="png", use_pos=False):
         graph_draw(G, output=file_name + "." + ext)
 
 
-def get_edges(n1, n2, n):
-    all_valid_nodes = get_valid_nodes(n1, n2, n)
-    unique_valid_nodes, reverse_index = np.unique(all_valid_nodes, return_inverse=True)
-    return unique_valid_nodes, reverse_index
-
-
-def extract_graphs_from_unique_nodes(
-    unique_nodes_path, geo_path, links_path, assets_path, file_logger
-):
-    as_names = [
-        name for name in os.listdir(unique_nodes_path) if name.split(".")[-1] == "csv"
-    ]
-    for as_name_file in as_names:
-        as_name = as_name_file.split(".")[0]
-        node_locations = pd.read_hdf(
-            geo_path,
-            "geo",
-            columns=["id", "latitude", "longitude"],
-            where=["ases=='{}'".format(as_name)],
+def information_from_as(geo_path, links_path, interfaces_path, as_name):
+    as_interfaces_path = os.path.join(interfaces_path, as_name + ".csv")
+    as_links_path = os.path.join(links_path, as_name + ".csv")
+    node_locations = pd.read_hdf(
+        geo_path,
+        "geo",
+        columns=["id", "latitude", "longitude"],
+        where=["ases=='{}'".format(as_name)],
+    )
+    if node_locations.shape[0] == 0:
+        return -1
+    try:
+        links_df = pd.read_csv(as_links_path, index_col=False, header=None, dtype=str)
+        links_df.columns = ["n1", "n2", "ip1", "ip2"]
+    except FileNotFoundError:
+        return -2
+    try:
+        interfaces_df = pd.read_csv(
+            as_interfaces_path, index_col=False, header=None, dtype=str
         )
-        as_links_path = os.path.join(links_path, as_name_file)
-        as_nodes_path = os.path.join(unique_nodes_path, as_name_file)
-        try:
-            links_df = pd.read_csv(
-                as_links_path, index_col=False, header=None, dtype=str
-            )
-            links_df.columns = ["n1", "n2", "ip1", "ip2"]
-            node_ids = links_df[["n1", "n2"]].applymap(lambda s: s[1:])
-            nodes_1 = node_ids["n1"].values.squeeze().astype(int)
-            nodes_2 = node_ids["n2"].values.squeeze().astype(int)
-        except FileNotFoundError:
-            file_logger.warning(
-                "AS {} does not have any link.".format(as_name_file.split(".")[0])
-            )
-            continue
-        with open(as_nodes_path, "r") as f:
-            k = 0
-            for node_group in f:
-                node_group = node_group.rstrip()
-                node_group = node_group.split(",")
-                node_group = np.array(list(map(lambda s: int(s[1:]), node_group)))
-                nodes, flat_edges = get_edges(nodes_1, nodes_2, node_group)
+        interfaces_df.columns = ["i", "n"]
+    except FileNotFoundError:
+        return -3
+    return node_locations, links_df, interfaces_df
 
-                if len(nodes) == 0:
-                    print("Skip line {}".format(k))
-                    continue
-                print(len(nodes))
 
-                query_node_locations = node_locations.loc[
-                    node_locations["id"].isin(
-                        ["N{}".format(node_id) for node_id in nodes]
-                    )
-                ]
-                query_node_locations = query_node_locations.sort_values("id")
-                locations = query_node_locations.loc[
-                    :, ("latitude", "longitude")
-                ].values
-                edges = np.stack(
-                    (
-                        flat_edges[0 : len(flat_edges) // 2],
-                        flat_edges[len(flat_edges) // 2 :],
-                    )
-                )
-                graph_path = os.path.join(assets_path, "{}-{}".format(as_name, k))
-                draw_graph(locations, edges, graph_path)
-                k += 1
+def get_edges(nodes, links, labels):
+    nodes["labels"] = labels
+    nodes = nodes.set_index("id")
+    link_labels = links.loc[:, ("n1", "n2")].applymap(
+        lambda s: nodes.loc[s, "labels"]
+    )
+    links["label1"] = link_labels["n1"]
+    links["label2"] = link_labels["n2"]
+    links = links.set_index(["n1", "n2"])
+    links_non_loops = links.loc[links["label1"] != links["label2"]]
+    links_non_loops_multi = links_non_loops.loc[
+        ~links_non_loops.duplicated(subset=["label1", "label2"])
+    ]
+    return links_non_loops_multi
+
+
+def get_interfaces(nodes, interfaces):
+    def to_binary(addrs):
+        binary_interfaces = np.zeros((len(addrs), 32), dtype=np.int8)
+        for i, str_ip in enumerate(addrs):
+            str_ip = str_ip.split(".")
+            int_ip = np.array(list(map(lambda s: int(s), str_ip)))
+            binary_ip = ((int_ip.reshape(-1, 1) & (1 << np.arange(8))) > 0).astype(np.int8)
+            binary_interfaces[i] = binary_ip.flatten()
+        return binary_interfaces
+
+    node_ids = nodes["id"]
+    interfaces_of_nodes = []
+    for node_id in node_ids:
+        node_addrs = interfaces.loc[interfaces["id"] == node_id, "addrs"].values
+        interfaces_of_nodes.append(to_binary(node_addrs))
+    return interfaces_of_nodes
 
 
 def extract_graphs_from_joining_nodes(
-    geo_path, links_path, assets_path, file_logger, add_noise
+    geo_path,
+    links_path,
+    interfaces_path,
+    assets_path,
+    file_logger,
+    add_noise,
+    minimum_nodes=30,
 ):
-    num_of_empty_ases = 0
-    num_of_non_link_ases = 0
-    num_of_small_unique_nodes_ases = 0
+    empty_ases = 0
+    small_ases = 0
+    non_link_ases = 0
+    non_interfaces_ases = 0
     ases = pd.read_hdf(geo_path, "ases").values.squeeze()
     for as_name in ases:
-        as_links_path = os.path.join(links_path, as_name + ".csv")
-        try:
-            links_df = pd.read_csv(
-                as_links_path, index_col=False, header=None, dtype=str
-            )
-            links_df.columns = ["n1", "n2", "ip1", "ip2"]
-        except FileNotFoundError:
-            num_of_non_link_ases += 1
-            continue
-
-        node_locations = pd.read_hdf(
-            geo_path,
-            "geo",
-            columns=["id", "latitude", "longitude"],
-            where=["ases=='{}'".format(as_name)],
-        )
-        if node_locations.shape[0] == 0:
-            num_of_empty_ases += 1
-            continue
+        inf_out = information_from_as(geo_path, links_path, interfaces_path, as_name)
+        if type(inf_out) is int:
+            if inf_out == -1:
+                empty_ases += 1
+                continue
+            elif inf_out == -2:
+                non_link_ases += 1
+                continue
+            else:
+                non_interfaces_ases += 1
+                continue  # TODO: generate synthetic interfaces optionally
+        node_locations, links, interfaces = inf_out
 
         if add_noise:
             X = node_locations.loc[:, ("latitude", "longitude")].values
             noised_X, labels, nodes, _, _ = get_unique_index(X, add_noise)
             node_locations.loc[:, ("latitude", "longitude")] = noised_X
-            print(X.shape, noised_X.shape)
         else:
             labels, nodes, _, _ = get_unique_index(
                 node_locations.loc[:, ("latitude", "longitude")].values
             )
         if nodes.shape[0] < 20:
-            num_of_small_unique_nodes_ases += 1
+            small_ases += 1
             continue
-
-        node_locations["labels"] = labels
-        node_locations = node_locations.set_index("id")
-        link_labels = links_df.loc[:, ("n1", "n2")].applymap(
-            lambda s: node_locations.loc[s, "labels"]
-        )
-        links_df["label1"] = link_labels["n1"]
-        links_df["label2"] = link_labels["n2"]
-        links_df = links_df.set_index(["n1", "n2"])
-        links_non_loops = links_df.loc[links_df["label1"] != links_df["label2"]]
-        links_non_loops_multi = links_non_loops.loc[
-            ~links_non_loops.duplicated(subset=["label1", "label2"])
-        ]
+        
+        links_non_loops_multi = get_edges(node_locations, links, labels)
+        interfaces_of_nodes = get_interfaces(node_locations, interfaces)
 
         n_nodes = nodes.shape[0]
         all_n_nodes = labels.shape[0]
         n_links = links_non_loops_multi.shape[0]
-        all_n_links = links_df.shape[0]
+        all_n_links = links.shape[0]
         file_logger.info(
-            "For AS {},  {} nodes was used of {}, and {} links was used of {}.".format(
+            "For AS {},  {} / {} of nodes and {} / {} of links.".format(
                 as_name, n_nodes, all_n_nodes, n_links, all_n_links
             )
         )
@@ -190,27 +148,26 @@ def extract_graphs_from_joining_nodes(
         draw_graph(locations, edges, graph_path, ext="pdf")
 
         file_logger.info(
-            "{} ASes do not have any node with geolocation position."
-            "{} ASes with number of nodes with different positions < 20".format(
-                num_of_small_unique_nodes_ases, num_of_empty_ases
+            "{} ASes do not present at least {} distinguish geolocated nodes;"
+            "{} ASes do not have geolocated nodes; "
+            "{} ASes do not have links; "
+            "{} ASes do not have interfaces".format(
+                small_ases,
+                minimum_nodes,
+                empty_ases,
+                non_link_ases,
+                non_interfaces_ases,
             )
         )
 
 
-def create_graphs_from_ases(
-    geo_path, links_path, unique_nodes_path=None, add_noise=True
-):
+def create_graphs_from_ases(geo_path, links_path, interfaces_path, add_noise=True):
     assets_path = os.path.join("data", "draws")
     os.makedirs(assets_path, exist_ok=True)
     file_logger = create_logger("graphs.log")
-    if unique_nodes_path is not None:
-        extract_graphs_from_unique_nodes(
-            unique_nodes_path, geo_path, links_path, assets_path, file_logger
-        )
-    else:
-        extract_graphs_from_joining_nodes(
-            geo_path, links_path, assets_path, file_logger, add_noise
-        )
+    extract_graphs_from_joining_nodes(
+        geo_path, links_path, interfaces_path, assets_path, file_logger, add_noise
+    )
 
 
 create_graphs_from_ases(
